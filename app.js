b481148c2ffa8a7b8316a480f75d126f3e31714a
ttdjohns <32712899+ttdjohns@users.noreply.github.com>
2018-11-21 16:55:26 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-﻿'use strict';
+'use strict';
 const mysql = require('mysql');
 var debug = require('debug');
 var express = require('express');
@@ -15,9 +15,9 @@ var app = express();
 app.use(bodyParser.json());
 
 const DBPORT = 3306;
-const ADMIN_PERMISSION_LEVEL = 2;
-const TEAM_MANAGER_PERMISSION_LEVEL = 1;
-const WORKER_PERMISSION_LEVEL = 0;
+const ADMIN_PERMISSION_LEVEL = 3;
+const TEAM_MANAGER_PERMISSION_LEVEL = 2;
+const WORKER_PERMISSION_LEVEL = 1;
 /*
  * request
  *      { "id": 1 }
@@ -239,15 +239,15 @@ app.post('/editEmail', async function (req, res) {
                 };
                 resolve(true);
             });
-            async function f(p1) {
+            async function f(p1, p2) {
 
                 let p1V = await p1;
                 let p2V = await p2;
                 connection.end();
                 console.log('connection closed');
-                res.send({ status: p1V });
+                res.send({ status: (p1V && p2V) });
             }
-            f(p1);
+            f(p1, p2);
         }
         else { res.send({ status: false }) };
     }
@@ -314,7 +314,7 @@ app.post('/editPhoneNumbers', async function (req, res) {
                 };
                 resolve(true);
             });
-            async function f(p1) {
+            async function f(p1, p2) {
 
                 let p1V = await p1;
                 let p2V = await p2;
@@ -322,46 +322,12 @@ app.post('/editPhoneNumbers', async function (req, res) {
                 console.log('connection closed');
                 res.send({ "status": (p1V && p2V) });
             }
-            f(p1);
+            f(p1, p2);
         }
         else { res.send({ "status": false }) };
     }
     else {
         res.send({ "status": false });
-    }
-});
-
-
-/*
- * request
- *     
- *
- * response
- * 
-*/
-app.post('/allWorkers', async function (req, res) {
-    // check for permissions
-    if (await verifyPermissions((req.body.id), ADMIN_PERMISSION_LEVEL)) {
-        console.log('permission granted');
-        // connect to db
-        var connection = connectToDB();
-        // q db
-        var str = 'select * from ProjectProDB.WORKERS';
-        connection.query(str, (err, rows) => {
-            if (err) {
-                console.log(err)
-                res.send("error");
-            }
-            else {
-                //console.log(rows);
-                res.send(rows);
-            };
-        });
-        connection.end();
-        console.log('connection closed in allWorkers');
-    }
-    else {
-        res.send("invalid permissions");
     }
 });
 
@@ -428,19 +394,25 @@ app.post('/viewWorkerStrengths', async function (req, res) {
  * request
  *      { "id": 1 }
  *
- * response
+ * response    
  *{
- *  "status": true,
+    "status": true,
     "Strengths": [
-        [   1,
-            "First Str",
-            ""
-        ],
-        [
-            2,
-            " 2 Str",
-            ""
-        ]
+        {
+            "Strength_ID": 1,
+            "Strength_name": "First Str",
+            "Strength_description": ""
+        },
+        {
+            "Strength_ID": 2,
+            "Strength_name": " 2 Str",
+            "Strength_description": ""
+        },
+        {
+            "Strength_ID": 3,
+            "Strength_name": "3 Str",
+            "Strength_description": ""
+        }
     ]
 }
 */
@@ -471,7 +443,11 @@ app.post('/viewAllStrengths', async function (req, res) {
             let p1V = await p1;
             var list = [];
             for (var i = 0; i < Object.keys(p1V).length; i++) {
-                list.push([p1V[i].Strength_ID, p1V[i].Strength_name, p1V[i].Strength_description]);
+                list.push({
+                    Strength_ID: p1V[i].Strength_ID,
+                    Strength_name: p1V[i].Strength_name,
+                    Strength_description: p1V[i].Strength_description
+                });
             }
             var ret = { "status": true, Strengths: list };
 
@@ -548,18 +524,17 @@ app.post('/getPermissionLevel', async function (req, res) {
 
 function verifyPermissions(id, reqLevel) {
     var con = connectToDB();
-    console.log('connection opened to verify permissions');
     var promise = new Promise(function (resolve, reject) {
         var str = 'select Access_level from ProjectProDB.ACCOUNT_ACCESS where Worker_ID = ' + id.toString();
         con.query(str, (err, rows) => {
             if (err) {
                 console.log(err);
-                resolve(false);
+                resolve(0);
             }
             else if ((rows[0].Access_level) >= (reqLevel)) {
-                resolve(true);
+                resolve(rows[0].Access_level);
             }
-            else { resolve(false); };
+            else { resolve(0); };
         });
     });
     //console.log("permssionResult is " + permissionResult);
@@ -615,3 +590,473 @@ app.post('/login', async function (req, res) {
     }
     f(p1);
 }); 
+
+/*
+ * request      (note that the Strength is the Strength_ID and the order indicates the rank. Strength[0] is your best)
+ *              (always must have 5 strengths)
+ *      { "id": 1,
+        "Strengths": [2, 3, 4, 5, 1]     
+        }
+ *
+ * response
+ * {
+    "status": true
+}
+*/
+app.post('/editWorkerStrengths', async function (req, res) {
+    // check for permissions
+    if ((req.body.Strengths.length == 5) && (await verifyPermissions((req.body.id), WORKER_PERMISSION_LEVEL))) {
+        console.log('permission granted');
+        var duplicates = false;
+        // ensure there are no duplicates in the new entries
+        for (var i = 0; i < req.body.Strengths.length; i++) {
+            for (var j = i + 1; j < req.body.Strengths.length; j++) {
+                if ((req.body.Strengths[i]) == (req.body.Strengths[j])) {
+                    duplicates = true;
+                };
+            }
+        }
+        if (!duplicates) {
+            // connect to db
+            var connection = connectToDB();
+            // q db
+
+            var p1 = new Promise(function (resolve, reject) {
+                var str = `delete from ProjectProDB.WORKER_HAS_STRENGTHS 
+                    where Worker_ID = ` + req.body.id + ';';
+                //console.log(str);
+                connection.query(str, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        resolve(false);
+                    }
+                    else {
+
+                        resolve(true);
+                    }
+                });
+            });
+
+            var p2 = new Promise(function (resolve, reject) {
+                for (var i = 0; i < req.body.Strengths.length; i++) {
+                    var str = `insert ProjectProDB.WORKER_HAS_STRENGTHS 
+                                values (` + req.body.id + ', ' + req.body.Strengths[i] + ', ' + i + ');';
+                    connection.query(str, (err, rows) => {
+                        if (err) {
+                            console.log(err);
+                            resolve(false);
+                        };
+                    });
+                };
+                resolve(true);
+            });
+            async function f(p1, p2) {
+
+                let p1V = await p1;
+                let p2V = await p2;
+                connection.end();
+                console.log('connection closed');
+                res.send({ status: (p1V && p2V) });
+            }
+            f(p1, p2);
+        }
+        else { res.send({ status: false }) };
+    }
+    else {
+        res.send({ status: false });
+    }
+}); 
+
+/*
+ * request     
+ *      { "id": 1    
+        }
+ *
+ * response    
+ * {
+    "status": true,
+    "Desires": [
+        {
+            "Task_ID": 1,
+            "Task_name": "weiof",
+            "Task_description": ""
+        },
+        {
+            "Task_ID": 2,
+            "Task_name": "asoidfna",
+            "Task_description": ""
+        },
+        {
+            "Task_ID": 3,
+            "Task_name": "fasiodfna",
+            "Task_description": ""
+        }
+    ]
+}
+*/
+app.post('/viewDesiredTasks', async function (req, res) {
+    // check for permissions
+    if (await verifyPermissions((req.body.id), WORKER_PERMISSION_LEVEL)) {
+        console.log('permission granted');
+        var connection = connectToDB();
+        var p1 = new Promise(function (resolve, reject) {
+            var str = `select t.Task_ID, t.Task_name, t.Task_description 
+                            from ((ProjectProDB.DESIRES as d) join (ProjectProDB.TASKS as t) on d.Task_ID = t.Task_ID)
+                            where d.Worker_ID = ` + req.body.id ;
+            //console.log(str);
+            connection.query(str, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    resolve([]);
+                }
+                else {
+
+                    resolve(rows);
+                }
+            });
+        });
+
+        async function f(p1) {
+            let p1V = await p1;
+            var list = [];
+            for (var i = 0; i < Object.keys(p1V).length; i++) {
+                list.push({
+                    Task_ID: p1V[i].Task_ID,
+                    Task_name: p1V[i].Task_name,
+                    Task_description: p1V[i].Task_description
+                });
+            }
+            var ret = { "status": true, Desires: list };
+
+            connection.end();
+            console.log('connection closed');
+            res.send(ret);
+        }
+        f(p1);
+    }
+    else { res.send({ "status": false }); };
+}); 
+
+/*
+ * request
+ *      { "id": 1 }
+ *
+ * response      
+ *{
+    "status": true,
+    "Tasks": [
+        {
+            "Task_ID": 1,
+            "Task_name": "weiof",
+            "Task_description": ""
+        },
+        {
+            "Task_ID": 2,
+            "Task_name": "asoidfna",
+            "Task_description": ""
+        }
+    ]
+}
+*/
+app.post('/viewAllTasks', async function (req, res) {
+    // check for permissions
+    if (await verifyPermissions((req.body.id), WORKER_PERMISSION_LEVEL)) {
+        console.log('permission granted');
+        // connect to db
+        var connection = connectToDB();
+        // q db
+
+        var p1 = new Promise(function (resolve, reject) {
+            var str = `select * from (ProjectProDB.TASKS as t);`;
+            //console.log(str);
+            connection.query(str, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    resolve([]);
+                }
+                else {
+                    resolve(rows);
+                }
+            });
+        });
+
+        async function f(p1) {
+            let p1V = await p1;
+            var list = [];
+            for (var i = 0; i < Object.keys(p1V).length; i++) {
+                list.push({
+                    Task_ID: p1V[i].Task_ID,
+                    Task_name: p1V[i].Task_name,
+                    Task_description: p1V[i].Task_description
+                });
+            }
+            var ret = { "status": true, Tasks: list };
+
+            connection.end();
+            console.log('connection closed');
+            res.send(ret);
+        }
+        f(p1);
+    }
+    else { res.send({ "status": false }); };
+}); 
+
+/*
+ * request          (Tasks is a list of desired Task_ID's)
+ *      { "id": 1,
+        "Tasks": [1, 2]
+        }
+ *
+ * response
+ * {
+    "status": true
+}
+*/
+app.post('/editWorkerDesires', async function (req, res) {
+    // check for permissions
+    if (await verifyPermissions((req.body.id), WORKER_PERMISSION_LEVEL)) {
+        console.log('permission granted');
+        var duplicates = false;
+        // ensure there are no duplicates in the new entries
+        for (var i = 0; i < req.body.Tasks.length; i++) {
+            for (var j = i + 1; j < req.body.Tasks.length; j++) {
+                if ((req.body.Tasks[i]) == (req.body.Tasks[j])) {
+                    duplicates = true;
+                };
+            }
+        }
+        if (!duplicates) {
+            // connect to db
+            var connection = connectToDB();
+            // q db
+
+            var p1 = new Promise(function (resolve, reject) {
+                var str = `delete from ProjectProDB.DESIRES
+                    where Worker_ID = ` + req.body.id + ';';
+                //console.log(str);
+                connection.query(str, (err, rows) => {
+                    if (err) {
+                        console.log(err);
+                        resolve(false);
+                    }
+                    else {
+
+                        resolve(true);
+                    }
+                });
+            });
+
+            var p2 = new Promise(function (resolve, reject) {
+                for (var i = 0; i < req.body.Tasks.length; i++) {
+                    var str = `insert ProjectProDB.DESIRES 
+                                values (` + req.body.id + ', \'' + req.body.Tasks[i] + '\');';
+                    connection.query(str, (err, rows) => {
+                        if (err) {
+                            console.log(err);
+                            resolve(false);
+                        };
+                    });
+                };
+                resolve(true);
+            });
+            async function f(p1, p2) {
+
+                let p1V = await p1;
+                let p2V = await p2;
+                connection.end();
+                console.log('connection closed');
+                res.send({ status: (p1V && p1V) });
+            }
+            f(p1, p2);
+        }
+        else { res.send({ status: false }) };
+    }
+    else {
+        res.send({ status: false });
+    }
+}); 
+
+/*
+ * request  
+ *      { "id": 2    
+        }
+ *
+ * response     
+ *              
+ * {
+    "status": true,
+    "Assigned_tasks": [
+        {
+            "Task_ID": 1,
+            "Task_name": "weiof",
+            "Task_description": "",
+            "Project_ID": 2,
+            "Project_name": "dream team",
+            "Project_description": "the best around"
+        },
+        {
+            "Task_ID": 2,
+            "Task_name": "asoidfna",
+            "Task_description": "",
+            "Project_ID": 2,
+            "Project_name": "dream team",
+            "Project_description": "the best around"
+        }
+    ]
+}
+*/
+app.post('/viewAssignedTasks', async function (req, res) {
+    // check for permissions
+    if (await verifyPermissions((req.body.id), WORKER_PERMISSION_LEVEL)) {
+        console.log('permission granted');
+        var connection = connectToDB();
+        var p1 = new Promise(function (resolve, reject) {
+            var str = `select t.Task_ID, t.Task_name, t.Task_description, p.Project_ID, p.Project_name, p.Project_description
+                            from (((ProjectProDB.PROJECT_TASKS as pt) join (ProjectProDB.TASKS as t) on pt.Task_ID = t.Task_ID)
+                                    join (ProjectProDB.PROJECTS as p) on pt.Project_ID = p.Project_ID) 
+                            where pt.Worker_ID = ` + req.body.id;
+            //console.log(str);
+            connection.query(str, (err, rows) => {
+                if (err) {
+                    console.log(err);
+                    resolve([]);
+                }
+                else { resolve(rows); }
+            });
+        });
+
+        async function f(p1) {
+            let p1V = await p1;
+            var list = [];
+            for (var i = 0; i < Object.keys(p1V).length; i++) {
+                list.push({
+                    Task_ID: p1V[i].Task_ID,
+                    Task_name: p1V[i].Task_name,
+                    Task_description: p1V[i].Task_description,
+                    Project_ID: p1V[i].Project_ID,
+                    Project_name: p1V[i].Project_name,
+                    Project_description: p1V[i].Project_description
+                });
+            }
+            var ret = { "status": true, Assigned_tasks: list };
+
+            connection.end();
+            console.log('connection closed');
+            res.send(ret);
+        }
+        f(p1);
+    }
+    else { res.send({ "status": false }); };
+}); 
+
+
+/*
+ * request { "id": 1 }
+ *     
+ *
+ * response 
+ * {
+    "status": true,
+    "Workers": [
+        {
+            "Worker_ID": 1,
+            "Hours_assigned": 1,
+            "Start_date": "2000-01-01T07:00:00.000Z",
+            "First_name": "Trent",
+            "Last_name": "Johnston",
+            "Worker_type": "Employee"
+        },
+        {
+            "Worker_ID": 2,
+            "Hours_assigned": 1,
+            "Start_date": "2000-01-01T07:00:00.000Z",
+            "First_name": "Arthur",
+            "Last_name": "L",
+            "Worker_type": "Employee"
+        }
+    ]
+}
+ * 
+*/
+app.post('/viewAllWorkers', async function (req, res) {
+    // check for permissions
+    if (await verifyPermissions((req.body.id), TEAM_MANAGER_PERMISSION_LEVEL)) {
+        console.log('permission granted');
+        // connect to db
+        var connection = connectToDB();
+        // q db
+        var str = 'select * from ProjectProDB.WORKERS';
+        connection.query(str, (err, rows) => {
+            if (err) {
+                console.log(err)
+                res.send({ status: false });
+            }
+            else {
+                var list = [];
+                for (var i = 0; i < Object.keys(rows).length; i++) {
+                    list.push({
+                        Worker_ID: rows[i].Worker_ID,
+                        Hours_assigned: rows[i].Hours_assigned,
+                        Start_date: rows[i].Start_date,
+                        First_name: rows[i].First_name,
+                        Last_name: rows[i].Last_name,
+                        Worker_type: rows[i].Worker_type
+                    });
+                }
+                res.send({ status: true, Workers: list });
+            };
+        });
+        connection.end();
+        console.log('connection closed in allWorkers');
+    }
+    else {
+        res.send({ status: false });
+    }
+});
+
+
+app.post('/addWorkerToTeam', async function (req, res) {
+    // check for permissions
+    var permission = await verifyPermissions((req.body.id), TEAM_MANAGER_PERMISSION_LEVEL)
+    console.log(permission);
+    if (permission) {
+        console.log('permission granted');
+        // connect to db
+        var connection = connectToDB();
+        // q db
+        var str = "";
+        if ((permission == TEAM_MANAGER_PERMISSION_LEVEL)) {
+            str = `insert into ProjectProDB.IS_PART_OF
+                    values (` + req.body.Worker_ID + `, (select Team_ID from ProjectProDB.TEAMS where Supervisor_ID = ` + req.body.id + '));';
+        }
+        else if ((permission == ADMIN_PERMISSION_LEVEL) && (req.body.Team_ID != undefined)) {
+            str = `insert into ProjectProDB.IS_PART_OF
+                    values (` + req.body.Worker_ID + `, ` + req.body.Team_ID + ');';
+        }
+        if (str != "") {
+            connection.query(str, (err, rows) => {
+                if (err) {
+                    console.log(err)
+                    res.send({ status: false });
+                }
+                else {
+                    res.send({ status: true });
+                };
+            });
+        }
+        else {
+            res.send({
+                status: false, 
+                error_message: "Error: organization manager must specify Team_ID"
+            })
+        }
+        connection.end();
+        console.log('connection closed in addWorkerToTeam');
+    }
+    else {
+        res.send({
+            status: false,
+            error_message: "Error: Invalid Permissions"
+        });
+    }
+});
